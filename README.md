3a946d51224c1f2f427bd27295d84074533f311b
ksator <khelil.sator@gmail.com>
2017-05-11 07:33:50 -0700

    m

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
-[![Build Status](https://travis-ci.org/ksator/openconfig-demo-in-ams.svg?branch=master)](https://travis-ci.org/ksator/openconfig-demo-in-ams)
+[![Build Status](https://travis-ci.org/ksator/openconfig-demo-with-juniper-devices.svg?branch=master)](https://travis-ci.org/ksator/openconfig-demo-with-juniper-devices)
 
 ### What to find on this repo:   
-Content about how to use Openconfig with Juniper devices.   
-I built it to run an Openconfig demo with Juniper devices (using OpenConfig BGP models).    
-It covers various options to interact with Juniper devices running Openconfig models:     
-- Devices CLI (Command Line Interface) (for Humans)  
-- NetConf in an SSH connection (low level operations)  
-- Network automation with Ansible (network automation with a high level of abstraction)   
-- Network automation with PyEZ (python library to interact with Junos)   
+Content about how to configure Juniper devices with Openconfig data models. It covers various options:
+- Devices Command Line Interface
+- NetConf in an SSH connection 
+- Network automation with Ansible
+- Network automation with PyEZ (python library to interact with Junos)
 
-So it includes also various automation content about using Openconfig with Juniper devices.  
-  
-It also covers basic usages of some nice tools for YANG:  
-- pyang  
-- pyangbind    
+So it also covers how to automate Openconfig on Juniper devices.  
+It also covers basic usages of YANG tools:
+- pyang
+- pyangbind
 
-### Doc
-We used this presentation before the demo [Network Automation with Ansible and OpenConfig YANG.pdf](https://github.com/ksator/openconfig-demo/blob/master/Network%20Automation%20with%20Ansible%20and%20OpenConfig%20YANG.pdf)
+### Doc: 
+A [presentation](https://github.com/ksator/openconfig-demo-with-juniper-devices/blob/master/Network%20Automation%20with%20Ansible%20and%20OpenConfig%20YANG.pdf) is at the root of the repo.   
+Please visit [wiki](https://github.com/ksator/openconfig-demo-with-juniper-devices/wiki) to get all the details regarding this project.  
 
-Please visit [**wiki**] (https://github.com/ksator/openconfig-demo/wiki) to get all the details regarding this project
-
-In order to download both the code and the wiki:  
+### How to clone this repo:
+In order to download both the code and the wiki: 
 ```
-git clone https://github.com/ksator/openconfig-demo.git
-git clone https://github.com/ksator/openconfig-demo.wiki.git
+git clone https://github.com/ksator/openconfig-demo-with-juniper-devices.git
+git clone https://github.com/ksator/openconfig-demo-with-juniper-devices.wiki.git
 ls
 ```
 
--- pyez.oc.bgp.py
+++ pyez.oc.bgp.py
@@ -5,14 +5,13 @@ from yaml import load
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from jnpr.junos.op.bgp import BGPNeighborTable
-from lxml import etree
-from lxml.builder import E
 import time
 import sys
+from lxml import etree
+from lxml.builder import E
 
-
-''' 
-argv[1] is the first command-line argument. 
+'''
+argv[1] is the first command-line argument.
 my_list_of_devices.yml is a yaml file with the list of devices. my_list_of_devices is a python variable (python list).
 '''
 f=open(sys.argv[1])
@@ -20,31 +19,49 @@ data=f.read()
 my_list_of_devices=load(data)
 f.close()
 
-#bgp.j2 is a jinja2 template to build BGP OC configuration file. template is object (instance of the class Template)
+#interfaces.j2 is a jinja2 template to build interfaces configuration file. template_interfaces is object (instance of the class Template)
+f=open('interfaces.j2')
+data=f.read()
+template_interfaces=Template(data)
+f.close()
+
+#bgp.j2 is a jinja2 template to build BGP OC configuration file. template_bgp is object (instance of the class Template)
 f=open('bgp.j2')
 data=f.read()
-template=Template(data)
+template_bgp=Template(data)
 f.close()
 
 # build from a jinja2 template the configuration file for each device
 print '\nBuilding the configuration file for each device from a jinja2 template:'
 for item in my_list_of_devices:
-    # get the variables definition for this item. item_vars is a python variable (dictionary) 
+    # dev_dictionnary is an empty dictionnary.
+    dev_dictionnary = {}
+    # get the variables definition for this item. item_vars is a python variable (dictionary)
+    # let's start with variables for interfaces
+    f=open('host_vars/'+item["host_name"]+'/topology.yml')
+    data=f.read()
+    item_vars=load(data)
+    # lets update dev_dictionnary with this
+    dev_dictionnary.update(item_vars)
+    f.close()
+    # let's add variables for BGP
     f=open('host_vars/'+item["host_name"]+'/bgp.yml')
     data=f.read()
     item_vars=load(data)
+    # lets update dev_dictionnary with this
+    dev_dictionnary.update(item_vars)
     f.close()
     # build the configuration file from a jinja2 template for this item using the variable item_vars
     print item["host_name"]+'.oc.bgp.conf'
     conffile=open(item["host_name"]+'.oc.bgp.conf','w')
-    conffile.write(template.render(item_vars))
+    conffile.write(template_bgp.render(dev_dictionnary))
     conffile.close()
 print 'Done for all the devices!\n'
 
 # load and commit the configuration file for each device
 print 'Pushing and committing the configuration files to the devices ...'
 for item in my_list_of_devices:
-    dev = Device(host=item["management_ip"], user='tiaddemo', password='OpenConfig')
+    dev = Device(host=item["management_ip"], user='lab', password='m0naco')
     dev.open()
     cfg=Config(dev)
     cfg.load(path=item["host_name"]+'.oc.bgp.conf', format='text')
@@ -55,7 +72,7 @@ for item in my_list_of_devices:
     dev.close()
 print 'Done for all the devices!\n'
 
-# Wait for 20 seconds. so BGP sessions will be established.  
+# Wait for 20 seconds. so BGP sessions will be established.
 print 'Waiting 20 seconds before starting to audit the BGP sessions state ...\n'
 time.sleep(20)
 
@@ -66,7 +83,7 @@ Please add table and view for bgp before as it is not provided by PyEZ.
 '''
 print 'Auditing the BGP states using PyEZ Tables and Views:'
 for device in my_list_of_devices:
-    dev = Device(host=device["management_ip"], user='tiaddemo', password='OpenConfig') 
+    dev = Device(host=device["management_ip"], user='lab', password='m0naco')
     dev.open()
     bgp=BGPNeighborTable (dev)
     bgp.get()
@@ -79,7 +96,7 @@ print 'Done for all the devices!\n'
 print 'Auditing the BGP states pulling state data using NetConf get as per standard (Junos render the state data in Openconfig format):' 
 rpc = E('get', E('filter', {'type': 'xpath', 'source': '/bgp'}))
 for item in my_list_of_devices:
-    dev = Device(host=item["management_ip"], user='tiaddemo', password='OpenConfig')
+    dev = Device(host=item["management_ip"], user='lab', password='m0naco')
     dev.open()
     print "Status of BGP sessions for device " + item["host_name"] + ':'
     op = dev.execute(rpc)
